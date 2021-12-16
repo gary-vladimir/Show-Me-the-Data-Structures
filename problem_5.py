982f2f0d68966c894f692edff179e9a7250f9bc1
gary-vladimir <gary@garybricks.com>
2021-12-15 21:48:26 -0600

    finished coding solution for problem 5

--- problem_5.py
+++ problem_5.py
@@ -7,7 +7,7 @@ class Linked_List:
         self.head = None
         self.tail = None
 
-    def append(self, value, time_stamp):
+    def append(self, time_stamp, value):
         if self.head is None:
             self.head = Block(time_stamp, value, 0)
             self.tail = self.head
@@ -22,10 +22,24 @@ class Block:
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
-        self.hash = self.calc_hash()
+        self.hash = self.calc_hash(data)
 
     def calc_hash(self, data):
         sha = hashlib.sha256()
         hash_str = data.encode('utf-8')
         sha.update(hash_str)
         return sha.hexdigest()
+
+
+def time_stamp_generator():
+    return datetime.datetime.utcnow().strftime("%d/%m/%Y %H:%M:%S")
+
+
+chain = Linked_List()
+chain.append(time_stamp_generator(), "Owls don't have eyeballs")
+chain.append(time_stamp_generator(), "In space, metal can weld on its own.")
+chain.append(time_stamp_generator(), "Cows kill more people than sharks.")
+
+print(chain.tail.data)
+print(chain.tail.previous_hash.data)
+print(chain.tail.previous_hash.previous_hash.data)
