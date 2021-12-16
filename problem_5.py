3e0bf6cb858afcf114b439a6909c61038de45a19
gary-vladimir <gary@garybricks.com>
2021-12-15 21:30:26 -0600

    Finished Linked_List class and added calc_hash as method to Block class

--- problem_5.py
+++ problem_5.py
@@ -1,20 +1,31 @@
+import datetime
 import hashlib
 
 
-def calc_hash(self):
-    sha = hashlib.sha256()
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
-    hash_str = "We are going to encode this string of data!".encode('utf-8')
-
-    sha.update(hash_str)
-
-    return sha.hexdigest()
+    def append(self, value, time_stamp):
+        if self.head is None:
+            self.head = Block(time_stamp, value, 0)
+            self.tail = self.head
+        else:
+            previous_hash = self.tail
+            self.tail = Block(time_stamp, value, previous_hash)
+            self.tail.previous_hash = previous_hash
 
 
 class Block:
-
     def __init__(self, timestamp, data, previous_hash):
         self.timestamp = timestamp
         self.data = data
         self.previous_hash = previous_hash
         self.hash = self.calc_hash()
+
+    def calc_hash(self, data):
+        sha = hashlib.sha256()
+        hash_str = data.encode('utf-8')
+        sha.update(hash_str)
+        return sha.hexdigest()
