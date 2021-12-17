042c000d1f070306421b12611fbb079c2d8212a5
gary-vladimir <gary@garybricks.com>
2021-12-17 12:30:53 -0600

    added two more test cases to problem 3

--- problem_3.py
+++ problem_3.py
@@ -38,7 +38,7 @@ def huffman_decoding(data, tree):
 if __name__ == "__main__":
     codes = {}
 
-    a_great_sentence = "The bird is the word"
+    a_great_sentence = "The bird is the word"  # <= test 1
 
     print("The size of the data is: {}\n".format(
         sys.getsizeof(a_great_sentence)))
@@ -56,7 +56,25 @@ if __name__ == "__main__":
         sys.getsizeof(decoded_data)))
     print("The content of the encoded data is: {}\n".format(decoded_data))
 
-    a_great_sentence = "another cool sentence"
+    a_great_sentence = "AAAAAB"  # <= test 2
+
+    print("The size of the data is: {}\n".format(
+        sys.getsizeof(a_great_sentence)))
+    print("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print("The size of the encoded data is: {}\n".format(
+        sys.getsizeof(int(encoded_data, base=2))))
+    print("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print("The size of the decoded data is: {}\n".format(
+        sys.getsizeof(decoded_data)))
+    print("The content of the encoded data is: {}\n".format(decoded_data))
+
+    a_great_sentence = " "  # <= test 3
 
     print("The size of the data is: {}\n".format(
         sys.getsizeof(a_great_sentence)))
