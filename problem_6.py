7becb125535e021f083f6b9f09d702976167dd9e
gary-vladimir <gary@garybricks.com>
2021-12-16 22:00:30 -0600

    added 1 more test case

--- problem_6.py
+++ problem_6.py
@@ -115,3 +115,19 @@ for i in element_2:
 
 print(union(linked_list_3, linked_list_4))
 print(intersection(linked_list_3, linked_list_4))
+
+
+linked_list_5 = LinkedList()
+linked_list_6 = LinkedList()
+
+element_1 = [1, 2, 3]
+element_2 = []
+
+for i in element_1:
+    linked_list_5.append(i)
+
+for i in element_2:
+    linked_list_6.append(i)
+
+print(union(linked_list_5, linked_list_6))
+print(intersection(linked_list_5, linked_list_6))
